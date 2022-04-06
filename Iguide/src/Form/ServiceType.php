d5324cb2728787ca105329a888d6dcb504af753c
eyatrabelsi <98555575+eyatrabelsi@users.noreply.github.com>
2022-04-06 00:19:51 +0000

    first_push

---
+++ Iguide/src/Form/ServiceType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Service;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ServiceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('nombreMax')
+            ->add('datedebut')
+            ->add('datefin')
+            ->add('cible')
+            ->add('lieu')
+            ->add('prix')
+            ->add('image')
+            ->add('nomcategorie')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Service::class,
+        ]);
+    }
+}
